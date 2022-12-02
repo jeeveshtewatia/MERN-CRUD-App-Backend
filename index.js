cf377a33376d0abcbc512fe0ba11df7cc45e8c84
jeevesh <jeeveshtewatia2@gmail.com>
2022-12-02 22:26:39 +0530

    added vercel

--- index.js
+++ index.js
@@ -18,12 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/tasks", taskRoutes);
 
-// const logger = (req, res, next) => {
-//   console.log("middleware working");
-//   console.log(req.method);
-//   next();
-// };
-
 //ROUTES
 app.get("/", (req, res) => {
   res.send("home  ");
@@ -39,15 +33,3 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
-
-// const startServer = async (req, res) => {
-//   try {
-//     await connectDB();
-//     app.listen(PORT, (req, res) => {
-//       console.log(`server is running in ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// startServer();
